c050fff4cec2b3160fcf048489cb454226859a6f
Jennifer Harden <jkfout1@gmail.com>
2019-03-09 21:29:27 -0600

    updated game wip

---
+++ assets/javascript/game.js
@@ -0,0 +1,171 @@
+$(document).ready(function () {
+
+    var potentialWords = [{
+        name: "indiana jones",
+        image: "./assets/images/movie/Indiana-Jones.jpg"
+    },
+    {
+        name: "star wars",
+        image: "./assets/images/movie/Star-Wars.jpg"
+    },
+    {
+        name: "harry potter",
+        image: "./assets/images/movie/Harry Potter.jpg"
+    },
+    {
+        name: "the lord of the rings",
+        image: "./assets/images/movie/LOTR.jpg"
+    },
+    {
+        name: "the fast and the furious",
+        image: "./assets/images/movie/Fast-and-Furious.jpg"
+    },
+    {
+        name: "back to the future",
+        image: "./assets/images/movie/Back to the Future.jpg"
+    },
+    {
+        name: "pirates of the caribbean",
+        image: "./assets/images/movie/Pirates-Caribbean.jpg"
+    },
+    {
+        name: "jurassic park",
+        image: "./assets/images/movie/jurassic-park.jpg"
+    },
+    {
+        name: "transformers",
+        image: "./assets/images/movie/Transformers.jpg"
+    },
+    {
+        name: "mission impossible",
+        image: "./assets/images/movie/mission_impossible.jpg"
+    }];
+
+    const maxGuesses = 14;
+    var pauseGame = false;
+
+    var lettersGuessed = [];
+    var shownMovies = [];
+    var guessingWord = [];
+    var wordToMatch;
+    var numberOfGuesses;
+    var wins = 0;
+
+    // Getting the initial movie data
+    resetGame()
+
+    // Awaiting key press event
+    document.onkeypress = function (event) {
+
+        // To make sure that the pressed key is a letter (converts it to upper case) and is the game not paused
+        if (isAlpha(event.key) && !pauseGame) {
+            checkForLetter(event.key.toUpperCase())
+        }
+    }
+    // Checks to see if the letter is in the word
+    function checkForLetter(letter) {
+        var letterFound = false;
+        var correctSoundEffect = document.createElement("audio");
+        var incorrectSoundEffect = document.createElement("audio");
+        correctSoundEffect.setAttribute("src", "assets/sounds/blop.mp3");
+        incorrectSoundEffect.setAttribute("src", "assets/sounds/ting.mp3");
+
+        //Searching to see if the letter is in the movie
+        for (var i = 0; i < wordToMatch.length; i++) {
+            if (letter === wordToMatch[i]) {
+                guessingWord[i] = letter
+                letterFound = true
+                correctSoundEffect.play()
+
+                // To see if guessed movie matches the selected movie
+                if (guessingWord.join("") === wordToMatch) {
+
+                    // To increase the number of wins
+                    wins++;
+                    wordToMatch = "";
+
+                    // This is the defined win state
+                    if (wins === 10) {
+                        var winSoundEffect = document.createElement("audio");
+                        winSoundEffect.setAttribute("src", "assets/sounds/winning.mp3")
+                        document.getElementById("hint").src = "./assets/images/awards.jpg";
+                        winSoundEffect.play();
+                    } else {
+                        pauseGame = true;
+                        setTimeout(resetGame, 4000);
+                    }
+                }
+            }
+        }
+
+
+        // If the letter is not found in the selected movie
+
+        if (!letterFound) {
+            incorrectSoundEffect.play()
+
+            // To check if the incorrect letter is already on the guessed letter list, if not, then push it to the list and decrease the number of guesses by 1
+            if (!lettersGuessed.includes(letter)) {
+                lettersGuessed.push(letter)
+                numberOfGuesses--
+            }
+            // If the number of guesses left = 0, then you lose
+            if (numberOfGuesses === 0) {
+                var loseSoundEffect = document.createElement("audio");
+                loseSoundEffect.setAttribute("src", "assets/sounds/loser.mp3")
+                document.getElementById("hint").src = "./assets/images/Loser.jpg";
+                loseSoundEffect.play();
+                guessingWord = wordToMatch.split()
+                pauseGame = true
+            }
+        }
+
+        updateDisplay()
+    }
+
+    // Checks to see if the key pressed is between a-z
+    function isAlpha(ch) {
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    // This initialize game variables and restarts the random movie selection
+    function resetGame() {
+        // To reset the arrays
+        lettersGuessed = []
+        guessingWord = []
+        numberOfGuesses = maxGuesses;
+        pauseGame = false;
+
+        for (var i = 0; i < potentialWords.length; i++) {
+            selectedMovie = potentialWords[Math.floor(Math.random() * potentialWords.length)];
+            if (shownMovies.indexOf(selectedMovie) < 0) {
+                shownMovies.push(selectedMovie);
+                break;
+            }
+        }
+        wordToMatch = selectedMovie.name.toUpperCase();
+        document.getElementById("hint").src = selectedMovie.image;
+
+
+        // To reset the guessed word
+        for (var i = 0; i < wordToMatch.length; i++) {
+            // To put a space instead of the _ between words of multiple display
+            if (wordToMatch[i] === " ") {
+                guessingWord.push(" ")
+            } else {
+                guessingWord.push(" _")
+            }
+        }
+
+        updateDisplay()
+    }
+
+    // This will update the display as gameplay advances
+    function updateDisplay() {
+        document.getElementById("wins").innerHTML = wins
+        document.getElementById("current-word").innerHTML = guessingWord.join("")
+        document.getElementById("guesses-left").innerHTML = numberOfGuesses
+        document.getElementById("letters-guessed").innerHTML = lettersGuessed.join(" ")
+    }
+
+});
